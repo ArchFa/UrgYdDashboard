2be8817cad009ce6faba859df58fd9a9874e248c
Artut Fattakhov <86257431+ArchFa@users.noreply.github.com>
2022-07-08 13:42:29 +0500

    add

--- Untitled-1.py
+++ Untitled-1.py
@@ -3,9 +3,31 @@
 
 import pandas as pd
 import streamlit as st
+import plotly.graph_objects as go
 
 # %%
 st.title("Отчет UrgY")
+
+# %%
+countries=['India', 'Australia',
+           'Japan', 'America',
+           'Russia']
+ 
+values = [4500, 2500, 1053, 500,
+          3200]
+
+# %%
+fig = go.Figure(
+    go.Pie(
+    labels = countries,
+    values = values,
+    hoverinfo = "label+percent",
+    textinfo = "value"
+))
+
+# %%
+st.header("Pie chart")
+st.plotly_chart(fig)
 
 # %%
 
